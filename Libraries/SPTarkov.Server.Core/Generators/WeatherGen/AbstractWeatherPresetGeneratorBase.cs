836112dc50c1f2d7336af13923d8aca84700b1fc
Chomp <27521899+chompDev@users.noreply.github.com>
2025-09-30 12:23:46 +0000

    Weather refactor (#596)
    * First pass at Weather generation refactor
    
    * Moved generation logic around
    
    * Added seasonal variability support
    
    * Expanded weather generation to use DI system and allow easier modding
    
    * Updated weather weight values
    
    Converted records into classes
    
    * Added fallback when generator isn't found
    
    * Fixed colliding lambda
    
    Added method comments
    
    * Cleanup of weather code
    
    * Adjusted `weatherPresetWeight` values
    
    ---------
    
    Co-authored-by: Chomp <dev@dev.sp-tarkov.com>

---
+++ Libraries/SPTarkov.Server.Core/Generators/WeatherGen/AbstractWeatherPresetGeneratorBase.cs
@@ -0,0 +1,46 @@
+using SPTarkov.Server.Core.Helpers;
+using SPTarkov.Server.Core.Models.Eft.Weather;
+using SPTarkov.Server.Core.Models.Enums;
+using SPTarkov.Server.Core.Models.Spt.Config;
+using SPTarkov.Server.Core.Models.Spt.Weather;
+using SPTarkov.Server.Core.Utils;
+
+namespace SPTarkov.Server.Core.Generators.WeatherGen;
+
+public abstract class AbstractWeatherPresetGeneratorBase(WeightedRandomHelper weightedRandomHelper, RandomUtil randomUtil)
+    : IWeatherPresetGenerator
+{
+    public abstract bool CanHandle(WeatherPreset preset);
+
+    public abstract Weather Generate(PresetWeights weatherWeights);
+
+    protected WindDirection GetWeightedWindDirection(PresetWeights weather)
+    {
+        return weightedRandomHelper.GetWeightedValue(weather.WindDirection);
+    }
+
+    protected double GetWeightedClouds(PresetWeights weather)
+    {
+        return double.Parse(weightedRandomHelper.GetWeightedValue(weather.Clouds));
+    }
+
+    protected double GetWeightedWindSpeed(PresetWeights weather)
+    {
+        return double.Parse(weightedRandomHelper.GetWeightedValue(weather.WindSpeed));
+    }
+
+    protected double GetWeightedFog(PresetWeights weather)
+    {
+        return double.Parse(weightedRandomHelper.GetWeightedValue(weather.Fog));
+    }
+
+    protected double GetWeightedRain(PresetWeights weather)
+    {
+        return double.Parse(weightedRandomHelper.GetWeightedValue(weather.Rain));
+    }
+
+    protected double GetRandomDouble(double min, double max, int precision = 3)
+    {
+        return Math.Round(randomUtil.GetDouble(min, max), precision);
+    }
+}
