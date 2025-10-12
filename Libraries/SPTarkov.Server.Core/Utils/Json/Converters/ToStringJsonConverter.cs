b416d7109a8b6faaa71fd2e9413723ab14db3bec
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-10-11 22:44:27 -0700

    Fix launcher not showing mods Convert `Version` and `SptVersion` to strings when serializing JSON for AbstractModMetadata

---
+++ Libraries/SPTarkov.Server.Core/Utils/Json/Converters/ToStringJsonConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SPTarkov.Server.Core.Utils.Json.Converters;
+
+internal class ToStringJsonConverter<T> : JsonConverter<T>
+{
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotSupportedException($"Deserialization of {typeof(T).Name} from string is not supported.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        if (value == null)
+        {
+            writer.WriteNullValue();
+        }
+        else
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
