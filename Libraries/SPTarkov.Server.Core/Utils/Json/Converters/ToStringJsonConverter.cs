1bdfd4186e193844d014201d3c67432a9828856b
DrakiaXYZ <565558+TheDgtl@users.noreply.github.com>
2025-10-11 22:55:44 -0700

    Implement ToStringJsonConverter Read method

--- Libraries/SPTarkov.Server.Core/Utils/Json/Converters/ToStringJsonConverter.cs
+++ Libraries/SPTarkov.Server.Core/Utils/Json/Converters/ToStringJsonConverter.cs
@@ -5,9 +5,27 @@ namespace SPTarkov.Server.Core.Utils.Json.Converters;
 
 internal class ToStringJsonConverter<T> : JsonConverter<T>
 {
-    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        throw new NotSupportedException($"Deserialization of {typeof(T).Name} from string is not supported.");
+        if (reader.TokenType == JsonTokenType.Null)
+        {
+            return default;
+        }
+
+        if (reader.TokenType == JsonTokenType.String)
+        {
+            var value = reader.GetString();
+            try
+            {
+                return (T?)Activator.CreateInstance(typeToConvert, value);
+            }
+            catch (Exception ex)
+            {
+                throw new JsonException($"Unable to convert \"{value}\" to {typeof(T).Name}.", ex);
+            }
+        }
+
+        throw new JsonException($"Expected string to deserialize {typeof(T).Name} but got {reader.TokenType}.");
     }
 
     public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
